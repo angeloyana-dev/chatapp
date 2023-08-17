7f682486dcb1ef8b3dd481c4eaa884efc5209ea8
angeloyana-dev <angeloyana.dev@gmail.com>
2023-08-18 03:25:11 +0800

    Added styles and dynamic function to online users list

---
+++ public/script.js
@@ -0,0 +1,11 @@
+
+
+// Dynamic DOM
+const usersList = document.getElementById("usersListContainer")
+function openUsersList(){
+	usersList.style.width = "65%"
+}
+
+function closeUsersList(){
+	usersList.style.width = "0"
+}
