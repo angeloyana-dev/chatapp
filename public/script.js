9aad7a591a2646814694fd8326893fbb7b0a71f8
angeloyana-dev <angeloyana.dev@gmail.com>
2023-08-18 15:40:51 +0800

    Created function that generates user message
    - Created a variables for message input and button
    - Created a functions that generates different types of user message

--- public/script.js
+++ public/script.js
@@ -1,3 +1,8 @@
+// Global variables
+const currentMsgWrapper = document.getElementById("current-msg-wrapper")
+const userInputMsg = document.getElementById("inputMsg")
+const sendMsgBtn = document.getElementById("sendMsgBtn")
+
 // Username prompt
 let username = window.prompt("Enter your username")
 let isValidUsername = false
@@ -13,6 +18,38 @@ while(!isValidUsername) {
 
 const socket = io()
 // Handle conversation here
+createGreet(username)
+// Testing
+createMsgContainer("user003", "Hello World!", "10:00pm", true)
+createMsgContainer("user030", "World Hello!", "10:01pm")
+
+// Messages generator
+function createGreet(username){
+	const container = document.createElement('div')
+	const wrapper = document.createElement('div')
+	      
+	container.classList.add('greet-msg-container')
+	wrapper.innerText = `Welcome to Chat app ${username}`
+	container.append(wrapper)
+	currentMsgWrapper.append(container)
+}
+
+function createMsgContainer(username, message, time, isUserMsg){
+	const container = document.createElement('div')
+	const wrapper = document.createElement('div')
+	const msgTextbox = document.createElement('p')
+	const usernameTextbox = document.createElement('span')
+	const timeTextbox = document.createElement('span')
+	
+	container.classList.add(isUserMsg ? "user-msg-container" : "others-msg-container")
+	msgTextbox.innerText = message
+	usernameTextbox.innerText = username
+	timeTextbox.innerText = time
+	
+	wrapper.append(usernameTextbox, msgTextbox, timeTextbox)
+	container.append(wrapper)
+	currentMsgWrapper.append(container)
+}
 
 // Dynamic DOM
 const usersList = document.getElementById("usersListContainer")
